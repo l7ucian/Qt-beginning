2a51367a69b7ad6f79d00753502dd9eb3fd37b34
Andercou Lucian Paul <lucian.andercou@gmail.com>
2016-10-07 17:36:04 +0300

    Widgets Layout

--- LayingOutWidgets/main.cpp
+++ LayingOutWidgets/main.cpp
@@ -1,11 +1,39 @@
-#include "mainwindow.h"
-#include <QApplication>
+#include<QApplication>
+#include<QHBoxLayout>
+#include<QSlider>
+#include<QSpinBox>
 
-int main(int argc, char *argv[])
+/*
+QSpinBox - rendered inside QWidget
+QSlider  - rendered inside QWidget
+QWidget  - main window
+*/
+
+int main(int argc, char*argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QApplication app(argc,argv);
 
-    return a.exec();
+    QWidget *window = new QWidget;
+    window->setWindowTitle("Enter your age");
+
+    QSpinBox *spinBox = new QSpinBox;
+    QSlider *slider = new QSlider(Qt::Horizontal);
+    spinBox->setRange(0, 130);
+    slider->setRange(0,130);
+
+    QObject::connect(spinBox, SIGNAL(valueChanged(int)),
+                     slider, SLOT(setValue(int)));
+    QObject::connect(slider, SIGNAL(valueChanged(int)),
+                     spinBox, SLOT(setValue(int)));
+    spinBox->setValue(35);
+
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(spinBox);
+    layout->addWidget(slider);
+    window->setLayout(layout);
+
+    window->show();
+
+    return app.exec();
+
 }
